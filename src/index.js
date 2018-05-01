fe22e5e2aa6fc78a282824b8657bb04809642225
Peter Munro <peter@petermunro.org>
2018-05-01 17:41:18 +0100

    Adds IntrospectionFragmentMatcher

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher
+} from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
+import introspectionQueryResultData from "./fragmentTypes.json";
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:4005/graphql" }),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({ fragmentMatcher })
 });
 
 ReactDOM.render(
